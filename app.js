c359f66fa743c7e1ef7763e8b69e0ac110e9c268
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-04-14 19:11:06 -0400

    url exceptions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ window.setInterval(function(){
 	if($('._2u0a').length < 1){
 		$("#fb-timeline-cover-name").prepend( "<div class='_2u0a'>Remembering <div>" )
 	}
+	console.log('everybodys dead');
 }, 500);
  
 
